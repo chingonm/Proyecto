72ab6c5727cb5731337a25615469e432d8cadd88
Markco <markco.molina@gmail.com>
2015-09-09 15:56:21 -0500

    Mensaje en la pagina home

--- home/home.php
+++ home/home.php
@@ -2,5 +2,10 @@
 <?php arriba();?>
 
 
+<div class="jumbotron">
+        <h1>Bienvenido</h1>
+        <p>Si deseas ver mas, te invitamos a que visites todos nuestros links</p>
+      </div>
+
 
 <?php abajo();?>
