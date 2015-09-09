84da2a3a339139f46796873c4244830637124315
bossuet <boargs@gmail.com>
2015-09-09 14:37:09 -0500

    added rutas de imagenes en carrusel

--- home/gallery.php
+++ home/gallery.php
@@ -7,7 +7,12 @@
     <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
     <li data-target="#carousel-example-generic" data-slide-to="1"></li>
     <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="5"></li>
   </ol>
+
+
 
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
@@ -18,11 +23,40 @@
       </div>
     </div>
     <div class="item">
-      <img src="..." alt="...">
-      <div class="carousel-caption">
-        ...
+        <img src="../img/Apple_Main.jpg" alt="Apple">
+        <div class="carousel-caption">
+          <h4>Apple.</h4>
+          <p>El lider de la inovacion.</p>
+        </div>
       </div>
-    </div>
+        <div class="item">
+        <img src="../img/Google_Main.jpg" >
+        <div class="carousel-caption">
+          <h4>Google.</h4>
+          <p>El Gigante de sillicon valley.</p>
+        </div>
+      </div>
+    <div class="item">
+        <img src="../img/android.jpg" >
+        <div class="carousel-caption">
+          <h4>Android</h4>
+          <p>Sistema operativo movil</p>
+        </div>
+      </div>
+    <div class="item">
+        <img src="../img/xbox.png" >
+        <div class="carousel-caption">
+          <h4>Xbox</h4>
+          <p>...</p>
+        </div>
+      </div>
+    <div class="item">
+        <img src="../img/ProdApple.jpg" >
+        <div class="carousel-caption">
+          <h4>ISomething</h4>
+          <p>Productos de apple</p>
+        </div>
+      </div>
     ...
   </div>
 
