f3393d6d0b1e79daf8961385b876a918c9efb13e
RIHCH <rihch888@gmail.com>
2015-09-09 08:16:56 -0500

    cambie de .html a .php

--- function/function.php
+++ function/function.php
@@ -16,9 +16,9 @@ function barraPrincipal() { ?>
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="home.html">Home <span class="sr-only">(current)</span></a></li>
-        <li><a href="gallery.html">Gallery</a></li>
-        <li><a href="about.html">About</a></li>
+        <li class=""><a href="home.php">Home</a></li>
+        <li class=""><a href="gallery.php">Gallery</a></li>
+        <li class=""><a href="about.php">About</a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
